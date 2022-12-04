f06e414bba8f70b80655d29f0abd8606dffffdce
PaulinaFinn <116019685+PaulinaFinn@users.noreply.github.com>
2022-12-04 13:28:38 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings:
+    """A class to save all the settings"""
+
+    def __init__(self):
+        "This will begin the code"
+
+        # This determines width and height of the entire thing
+        self.screen_width = 1200
+        self.screen_height = 600
+        # This states the colors of the background and the code from a color pallete
+        self.bg_color = (51,172,255)
+        self.bg_color2 = (255,255,255)
+
+        # Speed of the penguin
+        self.penguin_speed = 3
